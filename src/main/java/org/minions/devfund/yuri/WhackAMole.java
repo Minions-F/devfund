3f61b7fb56bd8008ead9ffb29982453ff270b3a6
Yuri Vayne <yurivayne@gmail.com>
2018-04-10 18:22:32 -0700

    initial commit

---
+++ src/main/java/org/minions/devfund/yuri/WhackAMole.java
@@ -0,0 +1,21 @@
+package org.minions.devfund.yuri;
+
+/**
+ * This class represents the wackamole game.
+ */
+public final class WhackAMole {
+
+    /**
+     * Constructor method.
+     */
+    private WhackAMole() {
+    }
+
+    /**
+     * Main method to execute the game.
+     *
+     * @param args arguments.
+     */
+    public static void main(final String[] args) {
+    }
+}
