65050d97dc3ed69f60117a6eaa7e242592c71ee1
Angela Valdez <rockstaradjm@gmail.com>
2018-04-12 16:13:38 -0400

    add tests

---
+++ src/test/java/org/minions/devfund/angela/WhackAMoleTest.java
@@ -0,0 +1,75 @@
+package org.minions.devfund.angela;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test for {@link WhackAMole}.
+ */
+public class WhackAMoleTest {
+
+    private static final int ATTEMPTS = 2;
+    private static final int GRID_DIMENSION = 10;
+    private WhackAMole whackAMole;
+
+
+    /**
+     * Preconditions.
+     */
+    @Before
+    public void setup() {
+        whackAMole = new WhackAMole(ATTEMPTS, GRID_DIMENSION);
+    }
+
+    /**
+     * Verify attempts reduced method.
+     */
+    @Test
+    public void main() throws FileNotFoundException {
+        System.setIn(new FileInputStream("input.txt"));
+        Main.main(new String[0]);
+    }
+
+    /**
+     * Verify attempts reduced method.
+     */
+    @Test
+    public void testAttemptsReduced() {
+        whackAMole.whack(0,0);
+        assertEquals(1, whackAMole.getAttempts());
+    }
+
+    /**
+     * Verify score increased method.
+     */
+    @Test
+    public void testScoreIncreased() {
+        whackAMole.place(0,0);
+        whackAMole.whack(0,0);
+        assertEquals(1, whackAMole.getScore());
+    }
+
+    /**
+     * Verify test place.
+     */
+    @Test
+    public void testPlace() {
+        assertTrue("Error placing.", whackAMole.place(1,1));
+        assertEquals(1, whackAMole.getMolesLeft());
+    }
+    
+    /**
+     * Verify test place.
+     */
+    @Test
+    public void prints(){
+        whackAMole.printGrid();
+        whackAMole.printGridToUser();
+    }
+}
