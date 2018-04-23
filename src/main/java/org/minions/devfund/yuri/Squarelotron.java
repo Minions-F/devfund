bf4c15019d1c5792c83bcfd3ab1b247e3084e55c
yvayne <yurivayne@gmail.com>
2018-04-23 00:44:14 -0400

    updated

--- src/main/java/org/minions/devfund/yuri/Squarelotron.java
+++ src/main/java/org/minions/devfund/yuri/Squarelotron.java
@@ -4,46 +4,75 @@ package org.minions.devfund.yuri;
  * Represents the squarelotron with all functionality.
  */
 public class Squarelotron {
-    private int[][] squarelotron;
-    private int size;
+  private int[][] squarelotron;
+  private int size;
 
-    /**
-     * Constructor Method.
-     * @param size matrix size.
-     */
-    public Squarelotron(final int size) {
-        squarelotron = new int[size][size];
-        this.size = size;
-        fillSquarelotron();
-    }
+  /**
+   * Constructor Method.
+   *
+   * @param size matrix size.
+   */
+  public Squarelotron(final int size) {
+    squarelotron = new int[size][size];
+    this.size = size;
+    fillSquarelotron();
+  }
 
-    /**
-     * Gets the squarelotron matrix.
-     *
-     * @return squarelotron matrix.
-     */
-    public int[][] getSquarelotron() {
-        return squarelotron.clone();
-    }
+  /**
+   * Gets the squarelotron matrix.
+   *
+   * @return squarelotron matrix.
+   */
+  public int[][] getSquarelotron() {
+    return squarelotron.clone();
+  }
 
-    /**
-     * Gets the squarelotron size.
-     *
-     * @return size.
-     */
-    public int getSize() {
-        return size;
-    }
+  /**
+   * Gets the squarelotron size.
+   *
+   * @return size.
+   */
+  public int getSize() {
+    return size;
+  }
 
-    /**
-     * Fills the squarelotron elements.
-     */
-    public void fillSquarelotron() {
-        int accumalator = 1;
-        for (int row = 0; row < size; row++) {
-            for (int column = 0; column < size; column++) {
-                    squarelotron[row][column] = accumalator++;
-            }
-        }
+  /**
+   * Fills the squarelotron elements.
+   */
+  public void fillSquarelotron() {
+    int accumalator = 1;
+    for (int row = 0; row < size; row++) {
+      for (int column = 0; column < size; column++) {
+        squarelotron[row][column] = accumalator++;
+      }
     }
+  }
+
+  /**
+   * Gets the rings amount of a matrix.
+   *
+   * @return the rings amount;
+   */
+  public int getRingsAmount() {
+    return size / 2;
+  }
+
+  /**
+   * Gets the center information for a matrix.
+   *
+   * @return <code>true</code> if the matrix has center element;
+   * <code>false</code> if it is not.
+   */
+  public boolean hasCenterElement() {
+    return size % 2 == 1;
+  }
+
+  /**
+   * Gets an specific ring of a matrix.
+   * @param ringNumber the ring number.
+   * @return an array that contains the elements of the ring.
+   */
+  public int[] getRing(final int ringNumber) {
+    return new int[]{};
+  }
 }
--- src/test/java/org/minions/devfund/yuri/SquarelotronTest.java
+++ src/test/java/org/minions/devfund/yuri/SquarelotronTest.java
@@ -4,32 +4,92 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 
 /**
  * Class in charge to test the functionality of squarelotron.
  */
 public class SquarelotronTest {
 
-    /**
-     * Verifies that the squarelotron matrix was filled correctly.
-     */
-    @Test
-    public void verifyTheLastElementOfSquarelotronIsCorrect() {
-        final int size = 4;
-        final int[][] expectedResult = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        Squarelotron squarelotron = new Squarelotron(size);
-        int[][] actualResult = squarelotron.getSquarelotron();
-        assertArrayEquals(expectedResult, actualResult);
-    }
+  /**
+   * Verifies that the squarelotron matrix was filled correctly.
+   */
+  @Test
+  public void testTheElementsOfSquarelotron() {
+    final int size = 4;
+    final int[][] expectedResult = new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    Squarelotron squarelotron = new Squarelotron(size);
+    int[][] actualResult = squarelotron.getSquarelotron();
+    assertArrayEquals(expectedResult, actualResult);
+  }
 
-    /**
-     * Verifies squarelotron size configured is correct.
-     */
-    @Test
-    public void verifySquarelotronSizeIsCorrect() {
-        final int expectedResult = 5;
-        Squarelotron squarelotron = new Squarelotron(expectedResult);
-        final int actualResult = squarelotron.getSize();
-        assertEquals(expectedResult, actualResult);
-    }
+  /**
+   * Verifies squarelotron size configured is correct.
+   */
+  @Test
+  public void testSquarelotronSize() {
+    final int expectedResult = 5;
+    Squarelotron squarelotron = new Squarelotron(expectedResult);
+    final int actualResult = squarelotron.getSize();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * Verifies the rings amount for a matrix size two.
+   */
+  @Test
+  public void testGetRingsFromMatrixSizeTwo() {
+    final int expectedResult = 1;
+    final int size = 2;
+    Squarelotron squarelotron = new Squarelotron(size);
+    final int actualResult = squarelotron.getRingsAmount();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * Verifies the rings amount for a matrix size two.
+   */
+  @Test
+  public void testGetRingsFromMatrixSizeSeven() {
+    final int expectedResult = 3;
+    final int size = 7;
+    Squarelotron squarelotron = new Squarelotron(size);
+    final int actualResult = squarelotron.getRingsAmount();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * Verifies that the matrix size 3 has a center element.
+   */
+  @Test
+  public void testMatrixSizeTreeHasCenterElement() {
+    final int size = 3;
+    Squarelotron squarelotron = new Squarelotron(size);
+    assertTrue(squarelotron.hasCenterElement());
+  }
+
+  /**
+   * Verifies that the matrix size 3 has a center element.
+   */
+  @Test
+  public void testMatrixSizeFourHasCenterElement() {
+    final int size = 4;
+    Squarelotron squarelotron = new Squarelotron(size);
+    assertFalse(squarelotron.hasCenterElement());
+  }
+
+  /**
+   * Verifies that the values of First Ring is correct from a Matrix size four.
+   */
+  @Test
+  public void testGetRingOneOfMatrixSize4() {
+    final int size = 4;
+    final int ringNumber = 1;
+    final int[] expectedResult = new int[] {1, 2, 3, 4, 5, 8, 12, 16, 15, 14, 13, 9, 5};
+    Squarelotron squarelotron = new Squarelotron(size);
+    int[] actualResult = squarelotron.getRing(ringNumber);
+    assertArrayEquals(expectedResult, actualResult);
+  }
 }
