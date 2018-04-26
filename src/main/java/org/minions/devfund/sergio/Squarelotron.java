2299731df6bd6086a169ccd2b670680b20841315
Sergio Navarro <sergio.navarro@jalasoft.com>
2018-04-26 10:40:21 -0400

    Merge branch 'develop' of https://github.com/Minions-F/devfund into sergio/whackamole
    # Conflicts:
    #	src/test/java/org/minions/devfund/sergio/WhackAMoleTest.java

--- src/main/java/org/minions/devfund/sergio/Squarelotron.java
+++ src/main/java/org/minions/devfund/sergio/Squarelotron.java
@@ -2,11 +2,30 @@ package org.minions.devfund.sergio;
 
 import java.util.Arrays;
 
+/**
+ * Main methods for game Squarelotron.
+ *
+ * @author Sergio Navarro
+ * @since 4/20/2018
+ */
 public class Squarelotron {
 
+    /**
+     * Internal Array.
+     */
     private int[][] squarelotron;
+
+    /**
+     * The zize of the Square.
+     */
     private int size;
 
+    /**
+     * Constructor for Squarelotron.
+     * Fill new Square with default numbers.
+     *
+     * @param n - The size of the square.
+     */
     public Squarelotron(int n) {
         size = n;
         int num = 1;
@@ -18,14 +37,33 @@ public class Squarelotron {
         }
     }
 
-    public String showSquare(Squarelotron squarelotron) {
+    /**
+     * Returns the original Squarelotron.
+     *
+     * @return the Squarelotron.
+     */
+    final int[][] getOriginalSquareArray() {
+        return Arrays.copyOf(this.squarelotron, size);
+    }
+
+    /**
+     * Prints the given square.
+     *
+     * @param squarelotron - The current square.
+     * @return A String of the square.
+     */
+    public String showSquare(final int[][] squarelotron) {
+
+        final String unit = "0";
+        final int decimal = 10;
+
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (squarelotron.squarelotron[i][j] < 10) {
-                    stringBuilder.append("0");
+                if (squarelotron[i][j] < decimal) {
+                    stringBuilder.append(unit);
                 }
-                stringBuilder.append(squarelotron.squarelotron[i][j]);
+                stringBuilder.append(squarelotron[i][j]);
                 if (j != size - 1) {
                     stringBuilder.append(" ");
                 }
@@ -35,10 +73,23 @@ public class Squarelotron {
         return stringBuilder.toString().trim();
     }
 
+    /**
+     * Gets the number of the ring in the square.
+     *
+     * @return The number of rings.
+     */
     public int getNumberOfRings() {
         return size % 2 == 0 ? size / 2 : size / 2 + 1;
     }
 
+    /**
+     * Makes the logic on the ring, checking if the current number is in the ring or out the ring.
+     *
+     * @param i    - Represent the rows.
+     * @param j    - Represent the columns.
+     * @param ring - The number ring to work with.
+     * @return - True if the number (i,j) is only in the ring, false otherwise.
+     */
     public boolean ringCheck(int i, int j, int ring) {
         // Check if the current number is in the ring
         return (i == ring - 1 || j == ring - 1 || i == size - ring || j == size - ring)
@@ -46,67 +97,112 @@ public class Squarelotron {
                 && !((i <= ring - 2 || i >= size + 1 - ring || j <= ring - 2 || j >= size + 1 - ring));
     }
 
+    /**
+     * Calls the method that flips the square upsideDown.
+     *
+     * @param ring - The Ring number to work with.
+     * @return The processed Square.
+     */
     public Squarelotron upsideDownFlip(int ring) {
         Squarelotron squarelotronResult = new Squarelotron(size);
         squarelotronResult.squarelotron = upsideDownFlip(squarelotronResult.squarelotron, ring);
         return squarelotronResult;
     }
 
-    private  int[][] upsideDownFlip(final int[][] squarelotronArray, int ring) {
-        final int[][] auxArray = Arrays.copyOf(squarelotronArray, size);
-        for (int i = 0; i < size/2; i++) {
+    /**
+     * Makes the square flip upsideDown.
+     *
+     * @param squarelotronArray - The given square array.
+     * @param ring              - The ring to work with.
+     * @return The flipped square.
+     */
+    public int[][] upsideDownFlip(final int[][] squarelotronArray, int ring) {
+        final int[][] auxArray = new int[size][size];
+        for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (ringCheck(i, j, ring)) {
                     auxArray[i][j] = squarelotronArray[size - 1 - i][j];
+                } else {
+                    auxArray[i][j] = squarelotronArray[i][j];
                 }
             }
         }
         return auxArray;
     }
 
+    /**
+     * Calls the method that swaps the square by main diagonal.
+     *
+     * @param ring - The ring to work with.
+     * @return The processed Square.
+     */
     public Squarelotron mainDiagonalFlip(int ring) {
         Squarelotron squarelotronResult = new Squarelotron(size);
-        squarelotronResult.squarelotron = mainDiagonalFlip(squarelotronResult.squarelotron,ring);
+        squarelotronResult.squarelotron = mainDiagonalFlip(squarelotronResult.squarelotron, ring);
         return squarelotronResult;
     }
 
-    public int[][] mainDiagonalFlip(int[][] squarelotronArray, int ring) {
-        final int[][] auxArray = squarelotronArray.clone();
+    /**
+     * Makes the square swap by main diagonal.
+     *
+     * @param squarelotronArray - The given square.
+     * @param ring              - The ring to work whit.
+     * @return The Swapped square.
+     */
+    public int[][] mainDiagonalFlip(final int[][] squarelotronArray, int ring) {
+        final int[][] auxArray = new int[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (ringCheck(i, j, ring)) {
-                    squarelotronArray[i][j] = auxArray[j][i];
+                    auxArray[i][j] = squarelotronArray[j][i];
+                } else {
+                    auxArray[i][j] = squarelotronArray[i][j];
                 }
             }
         }
-        return squarelotronArray;
+        return auxArray;
     }
 
-    public Squarelotron rotateEast(int turns) {
-        Squarelotron squarelotronResult = new Squarelotron(size);
+    /**
+     * Makes the rotation to right according the number of turns.
+     *
+     * @param turns - Number of turns.
+     * @return The processed Array.
+     */
+    public int[][] rotateEast(int turns) {
         while (turns > 0) {
-            squarelotronResult.squarelotron = upsideDownFlip(squarelotronResult.squarelotron, 1);
-            squarelotronResult.squarelotron = mainDiagonalFlip(squarelotronResult.squarelotron, 1);
+            this.squarelotron = upsideDownFlip(this.squarelotron, 1);
+            this.squarelotron = mainDiagonalFlip(this.squarelotron, 1);
             turns--;
         }
-        return squarelotronResult;
+        return Arrays.copyOf(this.squarelotron, size);
     }
 
-//    public Squarelotron rotateWest(int turns) {
-//        Squarelotron squarelotronResult = new Squarelotron(size);
-//        while (turns < 0) {
-//            squarelotronResult.squarelotron = mainDiagonalFlip(squarelotronResult.squarelotron, 1);
-//            squarelotronResult = upsideDownFlip(squarelotronResult, 1);
-//            turns++;
-//        }
-//        return squarelotronResult;
-//    }
+    /**
+     * Makes the rotation to left according the number of turns.
+     *
+     * @param turns - Number of turns.
+     * @return - The processed Array.
+     */
+    public int[][] rotateWest(int turns) {
+        while (turns < 0) {
+            this.squarelotron = mainDiagonalFlip(this.squarelotron, 1);
+            this.squarelotron = upsideDownFlip(this.squarelotron, 1);
+            turns++;
+        }
+        return Arrays.copyOf(this.squarelotron, size);
+    }
 
-//    public void rotateRight(int numberOfTurns) {
-//        if (numberOfTurns >= 0) {
-//            showSquare(rotateEast(numberOfTurns));
-//            return;
-//        }
-//        showSquare(rotateWest(numberOfTurns));
-//    }
+    /**
+     * Makes the rotation according the value of the numberOfTurns, left or right.
+     *
+     * @param numberOfTurns - The number of turns.
+     */
+    public void rotateRight(int numberOfTurns) {
+        if (numberOfTurns >= 0) {
+            rotateEast(numberOfTurns);
+            return;
+        }
+        rotateWest(numberOfTurns);
+    }
 }
--- src/test/java/org/minions/devfund/sergio/SquarelotronTest.java
+++ src/test/java/org/minions/devfund/sergio/SquarelotronTest.java
@@ -6,127 +6,392 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Test methods for Squarelotron.
+ *
+ * @author Sergio Navarro
+ * @since 4/20/2018
+ */
 public class SquarelotronTest {
 
     private StringBuilder test = new StringBuilder();
     private Squarelotron squarelotron;
 
+    /**
+     * Tests the values in the square of 2x2.
+     */
     @Test
     public void testSetValuesForSize2x2() {
-        squarelotron = new Squarelotron(2);
+        final int size = 2;
+        squarelotron = new Squarelotron(size);
         test.append("01 02").append("\n")
-            .append("03 04");
-        assertEquals(squarelotron.showSquare(squarelotron), test.toString().trim());
+                .append("03 04");
+        assertEquals(squarelotron.showSquare(squarelotron.getOriginalSquareArray()), test.toString().trim());
     }
 
+    /**
+     * Tests the values in the square of 5x5.
+     */
     @Test
     public void testSetValuesForSize5x5() {
-        squarelotron = new Squarelotron(5);
+        final int size = 5;
+        squarelotron = new Squarelotron(size);
         test.append("01 02 03 04 05").append("\n")
-            .append("06 07 08 09 10").append("\n")
-            .append("11 12 13 14 15").append("\n")
-            .append("16 17 18 19 20").append("\n")
-            .append("21 22 23 24 25");
-        assertEquals(squarelotron.showSquare(squarelotron), test.toString().trim());
+                .append("06 07 08 09 10").append("\n")
+                .append("11 12 13 14 15").append("\n")
+                .append("16 17 18 19 20").append("\n")
+                .append("21 22 23 24 25");
+        assertEquals(squarelotron.showSquare(squarelotron.getOriginalSquareArray()), test.toString().trim());
     }
 
+    /**
+     * Tests the values in the square of 4x4.
+     */
     @Test
     public void testGetNumberOfRingsInA4x4Square() {
-        squarelotron = new Squarelotron(4);
-        int ringsExpected = 2;
+        final int size = 4;
+        squarelotron = new Squarelotron(size);
+        final int ringsExpected = 2;
         assertEquals(squarelotron.getNumberOfRings(), ringsExpected);
     }
 
+    /**
+     * Tests the values in the square of 5x5.
+     */
     @Test
     public void testGetNumberOfRingsInA5x5Square() {
-        squarelotron = new Squarelotron(5);
-        int ringsExpected = 3;
+        final int size = 5;
+        squarelotron = new Squarelotron(size);
+        final int ringsExpected = 3;
         assertEquals(squarelotron.getNumberOfRings(), ringsExpected);
     }
 
+    /**
+     * Tests the values in the square of 3x3.
+     */
     @Test
     public void testRingCheckTrueInA3x3Square() {
-        squarelotron = new Squarelotron(3);
-        assertTrue(squarelotron.ringCheck(1, 1,2));
+        final int size = 3;
+        squarelotron = new Squarelotron(size);
+        assertTrue(squarelotron.ringCheck(1, 1, 2));
     }
 
+    /**
+     * Tests the values in the square of 4x4.
+     */
     @Test
     public void testRingCheckFalseInA4x4Square() {
-        squarelotron = new Squarelotron(4);
-        assertFalse(squarelotron.ringCheck(0,0,2));
+        final int size = 4;
+        squarelotron = new Squarelotron(size);
+        assertFalse(squarelotron.ringCheck(0, 0, 2));
     }
 
+    /**
+     * Tests the values in the square of 5x5.
+     */
     @Test
     public void testUpsideDownFlipInA5x5SquareRingOne() {
-        squarelotron = new Squarelotron(2);
+        final int size = 5;
+        squarelotron = new Squarelotron(size);
         test.append("21 22 23 24 25").append("\n")
-            .append("16 07 08 09 20").append("\n")
-            .append("11 12 13 14 15").append("\n")
-            .append("06 17 18 19 10").append("\n")
-            .append("01 02 03 04 05");
-        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.upsideDownFlip(1)));
+                .append("16 07 08 09 20").append("\n")
+                .append("11 12 13 14 15").append("\n")
+                .append("06 17 18 19 10").append("\n")
+                .append("01 02 03 04 05");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.upsideDownFlip(1)
+                .getOriginalSquareArray()));
     }
 
+    /**
+     * Tests the values in the square of 6x6.
+     */
     @Test
     public void testUpsideDownFlipInA6x6SquareRingTwo() {
-        squarelotron = new Squarelotron(2);
+        final int size = 6;
+        squarelotron = new Squarelotron(size);
         test.append("01 02 03 04 05 06").append("\n")
-            .append("07 26 27 28 29 12").append("\n")
-            .append("13 20 15 16 23 18").append("\n")
-            .append("19 14 21 22 17 24").append("\n")
-            .append("25 08 09 10 11 30").append("\n")
-            .append("31 32 33 34 35 36");
-        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.upsideDownFlip(2)));
+                .append("07 26 27 28 29 12").append("\n")
+                .append("13 20 15 16 23 18").append("\n")
+                .append("19 14 21 22 17 24").append("\n")
+                .append("25 08 09 10 11 30").append("\n")
+                .append("31 32 33 34 35 36");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.upsideDownFlip(2)
+                .getOriginalSquareArray()));
     }
 
+    /**
+     * Tests the values in the square of 6x6.
+     */
     @Test
     public void testUpsideDownFlipIn6x6SquareRingThree() {
-        squarelotron = new Squarelotron(6);
+        final int size = 6;
+        final int ring = 3;
+        squarelotron = new Squarelotron(size);
         test.append("01 02 03 04 05 06").append("\n")
-            .append("07 08 09 10 11 12").append("\n")
-            .append("13 14 21 22 17 18").append("\n")
-            .append("19 20 15 16 23 24").append("\n")
-            .append("25 26 27 28 29 30").append("\n")
-            .append("31 32 33 34 35 36");
-        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.upsideDownFlip(3)));
+                .append("07 08 09 10 11 12").append("\n")
+                .append("13 14 21 22 17 18").append("\n")
+                .append("19 20 15 16 23 24").append("\n")
+                .append("25 26 27 28 29 30").append("\n")
+                .append("31 32 33 34 35 36");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.upsideDownFlip(ring)
+                .getOriginalSquareArray()));
     }
 
+    /**
+     * Tests the values of the processed square of the method upsideDownFlip.
+     */
+    @Test
+    public void testUpsideDownFlipInOverCharge() {
+        final int size = 5;
+        squarelotron = new Squarelotron(size);
+        test.append("21 22 23 24 25").append("\n")
+                .append("16 07 08 09 20").append("\n")
+                .append("11 12 13 14 15").append("\n")
+                .append("06 17 18 19 10").append("\n")
+                .append("01 02 03 04 05");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron
+                .upsideDownFlip(squarelotron.getOriginalSquareArray(), 1)));
+    }
+
+    /**
+     * Tests the values in the square of 2x2.
+     */
     @Test
     public void testMainDiagonalFlipIn2x2SquareRingOne() {
-        squarelotron = new Squarelotron(2);
+        final int size = 2;
+        squarelotron = new Squarelotron(size);
         test.append("01 03").append("\n")
-            .append("02 04");
-        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.mainDiagonalFlip(1)));
+                .append("02 04");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.mainDiagonalFlip(1)
+                .getOriginalSquareArray()));
     }
 
+    /**
+     * Tests the values in the square of 6x6.
+     */
     @Test
     public void testMainDiagonalFlipIn6x6SquareRingOne() {
-        squarelotron = new Squarelotron(6);
+        final int size = 6;
+        squarelotron = new Squarelotron(size);
         test.append("01 07 13 19 25 31").append("\n")
-            .append("02 08 09 10 11 32").append("\n")
-            .append("03 14 15 16 17 33").append("\n")
-            .append("04 20 21 22 23 34").append("\n")
-            .append("05 26 27 28 29 35").append("\n")
-            .append("06 12 18 24 30 36");
-        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.mainDiagonalFlip(1)));
+                .append("02 08 09 10 11 32").append("\n")
+                .append("03 14 15 16 17 33").append("\n")
+                .append("04 20 21 22 23 34").append("\n")
+                .append("05 26 27 28 29 35").append("\n")
+                .append("06 12 18 24 30 36");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.mainDiagonalFlip(1)
+                .getOriginalSquareArray()));
     }
 
+    /**
+     * Tests the values in the square of 4x4.
+     */
     @Test
     public void testMainDiagonalFlipIn4x4RingTwo() {
-        squarelotron = new Squarelotron(4);
+        final int size = 4;
+        squarelotron = new Squarelotron(size);
         test.append("01 02 03 04").append("\n")
-            .append("05 06 10 08").append("\n")
-            .append("09 07 11 12").append("\n")
-            .append("13 14 15 16");
-        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.mainDiagonalFlip(2)));
+                .append("05 06 10 08").append("\n")
+                .append("09 07 11 12").append("\n")
+                .append("13 14 15 16");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.mainDiagonalFlip(2)
+                .getOriginalSquareArray()));
     }
 
-//    @Test
-//    public void testRotateEastIn2x2OneTurn() {
-//        squarelotron = new Squarelotron(2);
-//        test.append("03 01").append("\n")
-//            .append("04 02");
-//        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.rotateEast(1)));
-//    }
+    /**
+     * Tests the values of the processed square of the method mainDiagonalFlip.
+     */
+    @Test
+    public void testMainDiagonalFlipOverCharge() {
+        final int size = 4;
+        squarelotron = new Squarelotron(size);
+        test.append("01 05 09 13").append("\n")
+                .append("02 06 07 14").append("\n")
+                .append("03 10 11 15").append("\n")
+                .append("04 08 12 16");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron
+                .mainDiagonalFlip(squarelotron.getOriginalSquareArray(), 1)));
+    }
 
+    /**
+     * Tests the values in the square of 2x2.
+     */
+    @Test
+    public void testRotateEastIn2x2OneTurn() {
+        final int size = 2;
+        squarelotron = new Squarelotron(size);
+        test.append("03 01").append("\n")
+                .append("04 02");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.rotateEast(1)));
+    }
+
+    /**
+     * Tests the values in the square of 4x4.
+     */
+    @Test
+    public void testRotateEastIn4x4TwoTurns() {
+        final int size = 4;
+        squarelotron = new Squarelotron(size);
+        test.append("16 15 14 13").append("\n")
+                .append("12 06 07 09").append("\n")
+                .append("08 10 11 05").append("\n")
+                .append("04 03 02 01");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.rotateEast(2)));
+    }
+
+    /**
+     * Tests the values in the square of 6x6.
+     */
+    @Test
+    public void testRotateEastIn6x6FourTurns() {
+        final int size = 6;
+        final int ring = 4;
+        squarelotron = new Squarelotron(size);
+        test.append("01 02 03 04 05 06").append("\n")
+                .append("07 08 09 10 11 12").append("\n")
+                .append("13 14 15 16 17 18").append("\n")
+                .append("19 20 21 22 23 24").append("\n")
+                .append("25 26 27 28 29 30").append("\n")
+                .append("31 32 33 34 35 36");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.rotateEast(ring)));
+    }
+
+    /**
+     * Tests the values in the square of 2x2.
+     */
+    @Test
+    public void testRotateWestIn2x2OneTurn() {
+        final int size = 2;
+        squarelotron = new Squarelotron(size);
+        test.append("02 04").append("\n")
+                .append("01 03");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.rotateWest(-1)));
+    }
+
+    /**
+     * Tests the values in the square of 4x4.
+     */
+    @Test
+    public void testRotateWestIn4x4TwoTurns() {
+        final int size = 4;
+        final int turns = -2;
+        squarelotron = new Squarelotron(size);
+        test.append("16 15 14 13").append("\n")
+                .append("12 06 07 09").append("\n")
+                .append("08 10 11 05").append("\n")
+                .append("04 03 02 01");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.rotateWest(turns)));
+    }
+
+    /**
+     * Tests the values in the square of 6x6.
+     */
+    @Test
+    public void testRotateWestIn6x6FourTurns() {
+        final int size = 6;
+        final int turns = -4;
+        squarelotron = new Squarelotron(size);
+        test.append("01 02 03 04 05 06").append("\n")
+                .append("07 08 09 10 11 12").append("\n")
+                .append("13 14 15 16 17 18").append("\n")
+                .append("19 20 21 22 23 24").append("\n")
+                .append("25 26 27 28 29 30").append("\n")
+                .append("31 32 33 34 35 36");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.rotateWest(turns)));
+    }
+
+    /**
+     * Tests the values of the method RotateRight.
+     */
+    @Test
+    public void testRotateRightWithPositiveTurns() {
+        final int size = 4;
+        squarelotron = new Squarelotron(size);
+        test.append("16 15 14 13").append("\n")
+                .append("12 06 07 09").append("\n")
+                .append("08 10 11 05").append("\n")
+                .append("04 03 02 01");
+        squarelotron.rotateRight(2);
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.getOriginalSquareArray()));
+    }
+
+    /**
+     * Tests the values of the method RotateRight.
+     */
+    @Test
+    public void testRotateRightWithNegativeTurns() {
+        final int size = 4;
+        final int turns = -2;
+        squarelotron = new Squarelotron(size);
+        test.append("16 15 14 13").append("\n")
+                .append("12 06 07 09").append("\n")
+                .append("08 10 11 05").append("\n")
+                .append("04 03 02 01");
+        squarelotron.rotateRight(turns);
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.getOriginalSquareArray()));
+    }
+
+    /**
+     * Tests the values in the square of 3x3.
+     */
+    @Test
+    public void testUpsideDownFlipDimensionThreeRingOne() {
+        final int size = 3;
+        final int ring = 1;
+        squarelotron = new Squarelotron(size);
+        test.append("07 08 09").append("\n")
+            .append("04 05 06").append("\n")
+            .append("01 02 03");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.upsideDownFlip(ring)
+                .getOriginalSquareArray()));
+    }
+
+    /**
+     * Tests the values in the square of 3x3.
+     */
+    @Test
+    public void testUpsideDownFlipDimensionThreeRingTwo() {
+        final int size = 3;
+        final int ring = 2;
+        squarelotron = new Squarelotron(size);
+        test.append("01 02 03").append("\n")
+            .append("04 05 06").append("\n")
+            .append("07 08 09");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.upsideDownFlip(ring)
+                .getOriginalSquareArray()));
+    }
+
+    /**
+     * Tests the values in the square of 5x5.
+     */
+    @Test
+    public void testMainDiagonalFlipIn5x5RingOne() {
+        final int size = 5;
+        final int ring = 1;
+        squarelotron = new Squarelotron(size);
+        test.append("01 06 11 16 21").append("\n")
+            .append("02 07 08 09 22").append("\n")
+            .append("03 12 13 14 23").append("\n")
+            .append("04 17 18 19 24").append("\n")
+            .append("05 10 15 20 25");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.mainDiagonalFlip(ring)
+                .getOriginalSquareArray()));
+    }
+
+    /**
+     * Tests the values in the square of 5x5.
+     */
+    @Test
+    public void testMainDiagonalFlipIn5x5RingTwo() {
+        final int size = 5;
+        final int ring = 2;
+        squarelotron = new Squarelotron(size);
+        test.append("01 02 03 04 05").append("\n")
+            .append("06 07 12 17 10").append("\n")
+            .append("11 08 13 18 15").append("\n")
+            .append("16 09 14 19 20").append("\n")
+            .append("21 22 23 24 25");
+        assertEquals(test.toString().trim(), squarelotron.showSquare(squarelotron.mainDiagonalFlip(ring)
+                .getOriginalSquareArray()));
+    }
 }
