7f089d854677051bd3a78982f1001f61a214a091
Angela Valdez <rockstaradjm@gmail.com>
2018-05-22 15:56:30 -0400

    remove unnused imports

--- src/test/java/org/minions/devfund/angela/moviedatabase/MovieTest.java
+++ src/test/java/org/minions/devfund/angela/moviedatabase/MovieTest.java
@@ -3,7 +3,6 @@ package org.minions.devfund.angela.moviedatabase;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
